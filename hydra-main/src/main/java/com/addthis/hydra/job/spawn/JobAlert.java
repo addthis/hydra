4e1086abb481a95cf49041d0e847f84d8ab1c9a9
ian <ian@addthis.com>
2014-01-07 10:43:58 -0500

    fix logger name

--- hydra-main/src/main/java/com/addthis/hydra/job/spawn/JobAlert.java
+++ hydra-main/src/main/java/com/addthis/hydra/job/spawn/JobAlert.java
@@ -35,7 +35,7 @@ import org.slf4j.LoggerFactory;
  */
 public class JobAlert implements Codec.Codable {
 
-    private static final Logger log = LoggerFactory.getLogger(JobAlertRunner.class);
+    private static final Logger log = LoggerFactory.getLogger(JobAlert.class);
 
     //Alert types
     public static final int ON_ERROR = 0;
