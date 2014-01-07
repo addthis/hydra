ec792ca3f2e9e56cf038a4339f779772d8049aae
Stewart Allen <stewart@clearspring.com>
2014-01-07 01:22:33 -0500

    defer alias path walking until a node needs to be created

--- hydra-task/src/main/java/com/addthis/hydra/task/output/tree/PathAlias.java
+++ hydra-task/src/main/java/com/addthis/hydra/task/output/tree/PathAlias.java
@@ -111,40 +111,33 @@ public final class PathAlias extends PathKeyValue {
 
     @Override
     public DataTreeNode getOrCreateNode(final TreeMapState state, final String name) {
-        String p[] = new String[path.length];
-        for (int i = 0; i < p.length; i++) {
-            p[i] = ValueUtil.asNativeString(path[i].getFilteredValue(state));
-        }
-        DataTreeNode alias = null;
-        if (peer) {
-            alias = DataTreeUtil.pathLocateFrom(state.current(), p);
-        } else if (relativeUp > 0) {
-            alias = DataTreeUtil.pathLocateFrom(state.peek(relativeUp), p);
-        } else {
-            alias = DataTreeUtil.pathLocateFrom(state.current().getTreeRoot(), p);
-        }
-        if (alias != null) {
-            final DataTreeNode finalAlias = alias;
-            DataTreeNodeInitializer init = new DataTreeNodeInitializer() {
-                // TODO - defer walking path until we know we need to create a new node
-                @Override
-                public void onNewNode(DataTreeNode child) {
-                    child.aliasTo(finalAlias);
+        return state.getOrCreateNode(name, new DataTreeNodeInitializer() {
+            @Override
+            public void onNewNode(final DataTreeNode child) {
+                String p[] = new String[path.length];
+                for (int i = 0; i < p.length; i++) {
+                    p[i] = ValueUtil.asNativeString(path[i].getFilteredValue(state));
                 }
-            };
-            if (debug > 0) {
-                debug(true);
+                DataTreeNode alias = null;
+                if (peer) {
+                    alias = DataTreeUtil.pathLocateFrom(state.current(), p);
+                } else if (relativeUp > 0) {
+                    alias = DataTreeUtil.pathLocateFrom(state.peek(relativeUp), p);
+                } else {
+                    alias = DataTreeUtil.pathLocateFrom(state.current().getTreeRoot(), p);
+                }
+                if (alias == null) {
+                    if (debug > 0) {
+                        debug(false);
+                    }
+                    if (log.isDebugEnabled() || debug == 1) {
+                        log.warn("alias fail, missing " + Strings.join(p, " / "));
+                    }
+                } else {
+                    child.aliasTo(alias);
+                }
             }
-            return state.getOrCreateNode(name, init);
-        } else {
-            if (debug > 0) {
-                debug(false);
-            }
-            if (log.isDebugEnabled() || debug == 1) {
-                log.warn("alias fail, missing " + Strings.join(p, " / "));
-            }
-            return null;
-        }
+        });
     }
 
     private synchronized void debug(boolean hit) {
