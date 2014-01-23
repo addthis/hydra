d65be04063f7189bf62ae2b093904837bea91b70
Stewart Allen <stewart@clearspring.com>
2014-01-23 10:19:16 -0500

    make defCountHit protected. update comment

--- hydra-task/src/main/java/com/addthis/hydra/task/output/tree/PathElement.java
+++ hydra-task/src/main/java/com/addthis/hydra/task/output/tree/PathElement.java
@@ -30,7 +30,7 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 /*
  * TODO clean up preCheck in element, event*
- * TODO add alias nodes/map that use 'hard' links across the tree for cross-indexes
+ * TODO add alias nodes/map that use 'hard' links across the tree for cross-indexes (doesn't PathAlias do this?)
  * TODO extend feature support (on/off) to data elements on nodes
  */
 
@@ -47,7 +47,7 @@ public abstract class PathElement implements Codec.Codable, TreeDataParent {
 
     protected static final Logger log = LoggerFactory.getLogger(PathElement.class);
     protected static final boolean debug = System.getProperty("hydra.path.debug", "0").equals("1");
-    private static final boolean defCountHit = System.getProperty("hydra.default.counthit", "1").equals("1");
+    protected static final boolean defCountHit = System.getProperty("hydra.default.counthit", "1").equals("1");
 
     private static ClassMap cmap = new ClassMap() {
         @Override
