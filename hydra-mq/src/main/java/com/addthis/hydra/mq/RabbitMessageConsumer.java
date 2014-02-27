eb558f31fba5fe4e28fd97ae322bd5e2a61b5526
Matt Abrams <abramsm@addthis.com>
2014-02-27 07:43:10 -0500

    minor - formatting

--- hydra-mq/src/main/java/com/addthis/hydra/mq/RabbitMessageConsumer.java
+++ hydra-mq/src/main/java/com/addthis/hydra/mq/RabbitMessageConsumer.java
@@ -74,8 +74,7 @@ public class RabbitMessageConsumer extends DefaultConsumer implements MessageCon
             }
         } catch (ClassNotFoundException e) {
             log.warn("[rabbitConsumer] error reading message", e);
-            }
-
+        }
     }
 
     public boolean addMessageListener(MessageListener hostMessageListener) {
