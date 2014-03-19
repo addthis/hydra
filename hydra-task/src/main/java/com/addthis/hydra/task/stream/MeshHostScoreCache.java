2d14b5c73604b5514dee762c2c1e46e2eb07d425
ian <ian@addthis.com>
2014-03-19 16:55:16 -0400

    poll time cutoff and metric for the score cache

--- hydra-task/src/main/java/com/addthis/hydra/task/stream/MeshHostScoreCache.java
+++ hydra-task/src/main/java/com/addthis/hydra/task/stream/MeshHostScoreCache.java
@@ -13,7 +13,7 @@
  */
 package com.addthis.hydra.task.stream;
 
-import java.io.InputStream;
+import java.io.ByteArrayInputStream;
 
 import java.util.HashMap;
 import java.util.concurrent.TimeUnit;
@@ -22,15 +22,19 @@ import com.addthis.basis.util.Bytes;
 import com.addthis.basis.util.Parameter;
 
 import com.addthis.meshy.MeshyClient;
+import com.addthis.meshy.service.stream.SourceInputStream;
 import com.addthis.meshy.service.stream.StreamSource;
 
 import com.google.common.cache.CacheBuilder;
 import com.google.common.cache.CacheLoader;
 import com.google.common.cache.LoadingCache;
 
-import org.slf4j.Logger;
+import com.yammer.metrics.Metrics;
+import com.yammer.metrics.core.Meter;
 
+import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+
 /**
  * A wrapper around a guava loading cache for mesh host stats. The current stat implementation is just the number of
  * open files that a mesh node has (as provided by the meshy stats virtual file end point).
@@ -43,9 +47,15 @@ public class MeshHostScoreCache {
     // Time to wait before refreshing the meshy stats of a mesh node (in milliseconds). Is not done in the background
     private static final int refreshTime = Parameter.intValue("source.mesh.score.refresh", 10000);
 
+    // Time to wait before giving up on a score assessment and making up something bad (in milliseconds)
+    private static final int pollTime = Parameter.intValue("source.mesh.score.poll", 1000);
+
     // Amount to add to the score of each mesh node. Effectively says they have x more open files than they really do.
     // Increases randomness among low file counts.
     private static final int scoreFudge = Parameter.intValue("source.mesh.score.fudge", 2);
+
+    private static final Meter loadProblems = Metrics.newMeter(MeshHostScoreCache.class, "loadProblems", "loadProblems",
+            TimeUnit.SECONDS);
 
     private final LoadingCache<String, Integer> meshCache;
 
@@ -55,9 +65,16 @@ public class MeshHostScoreCache {
                 .build(
                         new CacheLoader<String, Integer>() {
                             public Integer load(String host) throws Exception {
-                                try (InputStream in = new StreamSource(meshLink, host, "/meshy/statsMap", 0).getInputStream()) {
+                                try (SourceInputStream meshSource
+                                             = new StreamSource(meshLink, host, "/meshy/statsMap", 0).getInputStream()) {
+                                    byte[] response = meshSource.poll(pollTime, TimeUnit.MILLISECONDS);
+                                    if (response == null) {
+                                        loadProblems.mark();
+                                        return 50;
+                                    }
+                                    ByteArrayInputStream in = new ByteArrayInputStream(response);
                                     int count = Bytes.readInt(in);
-                                    HashMap<String, Integer> stats = new HashMap<>();
+                                    HashMap<String, Integer> stats = new HashMap<>(1);
                                     while (count-- > 0) {
                                         String key = Bytes.readString(in);
                                         Integer val = Bytes.readInt(in);
@@ -73,10 +90,9 @@ public class MeshHostScoreCache {
         try {
             return meshCache.get(host);
         } catch (Exception e) {
-            if (log.isDebugEnabled()) {
-                log.debug("", e);
-            }
+            log.debug("", e);
             // If an unexpected error occurs, say that the host has about 50 open files as a safe case
+            loadProblems.mark();
             return 50;
         }
     }
