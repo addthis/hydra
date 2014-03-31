e709766a200490dc453f63099aec36b0cac432fe
hees <hees@addthis.com>
2014-03-31 13:08:48 -0400

    make all dependency text render rightside-up

--- hydra-main/web/spawn2/js/modules/graph.js
+++ hydra-main/web/spawn2/js/modules/graph.js
@@ -18,56 +18,6 @@ define([
 ],
 function(
 ){
-    var TreeDependencyGraphView = Backbone.View.extend({
-        initialize:function(){
-        },
-        render:function(){
-            var diameter = 760;
-            var tree = d3.layout.tree()
-                .size([360, diameter / 2 - 120])
-                .separation(function(a, b) {
-                    return (a.parent == b.parent ? 1 : 2) / a.depth;
-                });
-            var diagonal = d3.svg.diagonal.radial()
-                .projection(function(d) {
-                    var x = (d.x/180.0)*Math.PI;
-                    return [d.y, (d.x / 180) * Math.PI];
-                });
-            var svg = d3.select(this.$el.get(0)).append("svg")
-                .attr("width", diameter)
-                .attr("height", diameter - 150)
-                .append("g")
-                .attr("transform", "translate(" + diameter / 2 + "," + diameter / 2 + ")");
-            var nodes = tree.nodes(this.model.toJSON());
-            var links = tree.links(nodes);
-            var link = svg.selectAll(".link")
-                .data(links)
-                .enter().append("path")
-                .attr("class", "link")
-                .attr("d", diagonal);
-            var node = svg.selectAll(".node")
-                .data(nodes)
-                .enter().append("g")
-                .attr("class", "node")
-                .attr("transform", function(d) {
-                    return "rotate(" + (d.x - 90) + ")translate(" + d.y + ")";
-                });
-            node.append("circle")
-                .attr("r", 4.5);
-            node.append("text")
-                .attr("dy", ".31em")
-                .attr("text-anchor", function(d) {
-                    return d.x < 180 ? "start" : "end";
-                })
-                .attr("transform", function(d) {
-                    return d.x < 180 ? "translate(8)" : "rotate(180)translate(-8)";
-                })
-                .text(function(d) {
-                    return d.name;
-                });
-            return this;
-        }
-    });
     var TreeForceDirectedGraphView = Backbone.View.extend({
         initialize:function(){
             _.bindAll(this);
@@ -272,10 +222,10 @@ function(
             node.append("text")
                 .attr("dy", ".31em")
                 .attr("text-anchor", function(d) {
-                    return d.x < 180 ? "start" : "end";
+                    return "start";
                 })
                 .attr("transform", function(d) {
-                    return d.x < 180 ? "translate(8)" : "rotate(180)translate(-8)";
+                    return "rotate(" + (90 - d.x) + ")translate(8)"
                 })
                 .text(function(d) {
                     return d.name;
@@ -356,7 +306,6 @@ function(
     return {
         TreeForceDirectedGraphView:TreeForceDirectedGraphView,
         ForceDirectedGraphView:ForceDirectedGraphView,
-        TreeDependencyGraphVie:TreeDependencyGraphView,
         ForceDirectedGraphModel:ForceDirectedGraphModel,
         TreeGraphModel:TreeGraphModel,
         TreeDependencyGraphView:TreeDependencyGraphView
