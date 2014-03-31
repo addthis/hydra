1de70b567b8e38d34ed7ebf63de472605c2854a7
al <al@addthis.com>
2014-03-31 12:24:25 -0400

    Minions report meanActiveTasks as a percentage of their respective maxima

--- hydra-main/src/main/java/com/addthis/hydra/job/Minion.java
+++ hydra-main/src/main/java/com/addthis/hydra/job/Minion.java
@@ -778,7 +778,7 @@ public class Minion extends AbstractHandler implements MessageListener, Codec.Co
             minionStateLock.unlock();
         }
         status.setQueued(queued.toArray(new JobKey[queued.size()]));
-        status.setMeanActiveTasks(activeTaskHistogram.mean());
+        status.setMeanActiveTasks(activeTaskHistogram.mean() / (maxActiveTasks > 0 ? maxActiveTasks : 1));
         status.setMinionTypes(minionTypes);
         status.setUpdated();
         return status;
