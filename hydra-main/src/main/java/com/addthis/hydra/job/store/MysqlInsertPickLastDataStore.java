e1d8be5f8b7260107cb9aab995e44e0820ed8913
al <al@addthis.com>
2014-04-29 12:44:56 -0400

    Implemented insert-pick-last mysql data store, which is performing better than the old version.

---
+++ hydra-main/src/main/java/com/addthis/hydra/job/store/MysqlInsertPickLastDataStore.java
@@ -0,0 +1,277 @@
+package com.addthis.hydra.job.store;
+
+import javax.sql.rowset.serial.SerialBlob;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+import java.util.Properties;
+import java.util.Timer;
+import java.util.TimerTask;
+
+import java.sql.Blob;
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+
+import com.addthis.basis.util.Parameter;
+
+import com.mchange.v2.c3p0.ComboPooledDataSource;
+import com.ning.compress.lzf.LZFDecoder;
+import com.ning.compress.lzf.LZFEncoder;
+import com.ning.compress.lzf.LZFException;
+
+import org.slf4j.Logger;
+
+public class MysqlInsertPickLastDataStore implements SpawnDataStore {
+    private static final Logger log = org.slf4j.LoggerFactory.getLogger(MysqlInsertPickLastDataStore.class);
+
+    private static final String description = "mysqlinsertpicklast";
+    private final ComboPooledDataSource cpds;
+    /* The maximum allowable length for 'path' and 'child' values. */
+    protected static final int maxPathLength = Parameter.intValue("sql.datastore.max.path.length", 200);
+    /* Configuration parameters for the jdbc connection pool. */
+    private static final int minPoolSize = Parameter.intValue("sql.datastore.minpoolsize", 10);
+    private static final int maxPoolSize = Parameter.intValue("sql.datastore.maxpoolsize", 20);
+    /* How frequently to clean up duplicate values for the same key */
+    private static final int cleanupFrequencySeconds = Parameter.intValue("sql.datastore.cleanupseconds", 30);
+
+    /* Column names. In the standard implementations, path and child are VARCHAR(200) and value is a BLOB or TEXT. */
+    protected static final String pathKey = "path";
+    protected static final String valueKey = "val";
+    protected static final String childKey = "child";
+    protected static final String idKey = "id";
+    /* The simulated 'child' value used to store data about the parent. */
+    protected static final String blankChildId = "_root";
+    private final String queryTemplate;
+    private final String insertTemplate;
+    private final String deleteTemplate;
+    private final String getChildNamesTemplate;
+    private final String getChildrenTemplate;
+    private final String tableName;
+    private final String cleanupTemplate;
+
+    public MysqlInsertPickLastDataStore(String jdbcUrl, String tableName, Properties properties, boolean autoCleanup) throws Exception {
+        cpds = new ComboPooledDataSource();
+        cpds.setDriverClass("org.drizzle.jdbc.DrizzleDriver");
+        cpds.setJdbcUrl(jdbcUrl);
+        cpds.setInitialPoolSize(minPoolSize);
+        cpds.setMinPoolSize(minPoolSize);
+        cpds.setMaxPoolSize(maxPoolSize);
+        cpds.setProperties(properties);
+        this.tableName = tableName;
+        runStartupCommand();
+        queryTemplate = "SELECT " + valueKey + " FROM " + tableName + " WHERE " + pathKey + "=? AND " + childKey + "=? ORDER BY " +idKey + " DESC LIMIT 1";
+        insertTemplate = "INSERT INTO " + tableName + "(" + pathKey + "," + valueKey + "," + childKey + ") VALUES(?,?,?)";
+        deleteTemplate = "DELETE FROM " + tableName + " WHERE " + pathKey + "=? AND " + childKey + "=?";
+        getChildNamesTemplate = "SELECT DISTINCT " + childKey  + " FROM " + tableName + " WHERE " + pathKey + "=? AND " + childKey + "!=?";
+        getChildrenTemplate = "SELECT " + childKey +"," + valueKey  + " FROM " + tableName + " WHERE " + pathKey + "=? AND " + childKey + "!=? ORDER BY " + idKey; // SUPER HACKY. BASICALLY SO THE PUT COMMANDS WILL PUT THE MOST UP-TO-DATE THING IN LAST. THIS BLOWS!!!!!!!!
+        cleanupTemplate = "DELETE v FROM " + tableName + " AS v INNER JOIN " + tableName + " AS v2 ON (v." + pathKey+ ",v." + childKey + ") = (v2." + pathKey + ",v2." + childKey + ") AND v."+idKey + " < v2." + idKey; // v and v2 are dummy values; basically, delete all but the latest value for any (parent, child) combination. Not necessary for correctness, but should be run periodically to clean up data storage.
+        if (autoCleanup) {
+            new Timer("mysqldatastore_cleanup").scheduleAtFixedRate(new TimerTask() {
+                @Override
+                public void run() {
+                    try {
+                        cleanUp();
+                    } catch (SQLException e) {
+                        log.warn("Failed to perform mysql cleanup", e);
+                    }
+                }
+            }, cleanupFrequencySeconds, cleanupFrequencySeconds);
+
+        }
+
+    }
+
+    public void cleanUp() throws SQLException {
+        try (Connection connection = getConnection()) {
+            connection.prepareStatement(cleanupTemplate).execute();
+        }
+    }
+
+    public int countRows() throws SQLException {
+        try (Connection connection = getConnection()) {
+            ResultSet resultSet = connection.prepareStatement("SELECT COUNT(*) FROM " + tableName).executeQuery();
+            resultSet.next();
+            return resultSet.getInt(1);
+        }
+    }
+
+    private void runStartupCommand() throws SQLException {
+        try (Connection connection = getConnection()) {
+            connection.prepareStatement("CREATE TABLE IF NOT EXISTS " + tableName + "( "
+                                        + idKey + " MEDIUMINT NOT NULL AUTO_INCREMENT, "
+                                        + pathKey + " VARCHAR(" + maxPathLength + ") NOT NULL, "
+                                        + valueKey + " MEDIUMBLOB, "
+                                        + childKey + " VARCHAR(" + maxPathLength + "), "
+                                        + "PRIMARY KEY (" + idKey + "))"
+            ).execute();
+        }
+    }
+
+    protected Connection getConnection() throws SQLException {
+        return cpds.getConnection();
+    }
+
+    @Override
+    public String getDescription() {
+        return description;
+    }
+
+    @Override
+    public String get(String path) {
+        try {
+            return query(path, blankChildId);
+        } catch(NullPointerException npe ) {
+            return null;
+        } catch (SQLException e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+    protected static Blob valueToBlob(String value) throws SQLException {
+        return value != null ? new SerialBlob(LZFEncoder.encode(value.getBytes())) : null;
+    }
+
+    protected static String blobToValue(Blob blob) throws SQLException, LZFException {
+        return blob != null ? new String(LZFDecoder.decode(blob.getBytes(1l, (int) blob.length()))) : null;
+    }
+
+    @Override
+    public Map<String, String> get(String[] paths) {
+        // WHERE PATH=? OR PATH=? OR PATH=? IS FASTER
+        Map<String, String> rv = new HashMap<>();
+        for (String path : paths) {
+            String val = get(path);
+            if (val != null) {
+                rv.put(path, val);
+            }
+        }
+        return rv;
+    }
+
+    @Override
+    public void put(String path, String value) throws Exception {
+        insert(path, blankChildId, value);
+    }
+
+    private void insert(String path, String childId, String value)  throws SQLException {
+        try (Connection connection = getConnection()) {
+            PreparedStatement preparedStatement = connection.prepareStatement(insertTemplate);
+            preparedStatement.setString(1, path);
+            preparedStatement.setBlob(2, valueToBlob(value));
+            preparedStatement.setString(3, childId);
+            preparedStatement.execute();
+        }
+    }
+
+    private String query(String path, String childId) throws SQLException {
+        try (Connection connection = getConnection()) {
+            PreparedStatement preparedStatement = connection.prepareStatement(queryTemplate);
+            preparedStatement.setString(1, path);
+            preparedStatement.setString(2, childId);
+            ResultSet resultSet = preparedStatement.executeQuery();
+            boolean hasData = resultSet.next();
+            if (!hasData) {
+                return null;
+            } else {
+                return blobToValue(resultSet.getBlob(1));
+            }
+        } catch (LZFException e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+    private void delete(String path, String childId) throws SQLException {
+        try (Connection connection = getConnection()) {
+            PreparedStatement preparedStatement = connection.prepareStatement(deleteTemplate);
+            preparedStatement.setString(1, path);
+            preparedStatement.setString(2, childId);
+            preparedStatement.execute();
+        }
+    }
+
+    @Override
+    public void putAsChild(String parent, String childId, String value) throws Exception {
+        insert(parent, childId, value);
+    }
+
+    @Override
+    public String getChild(String parent, String childId) throws Exception {
+        return query(parent, childId);
+    }
+
+    @Override
+    public void deleteChild(String parent, String childId) {
+        try {
+            delete(parent, childId);
+        } catch (SQLException e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+    @Override
+    public void delete(String path) {
+        try {
+            delete(path, blankChildId);
+        } catch (SQLException e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+    @Override
+    public List<String> getChildrenNames(String path) {
+        try (Connection connection = getConnection()) {
+            PreparedStatement preparedStatement = connection.prepareStatement(getChildNamesTemplate);
+            preparedStatement.setString(1, path);
+            preparedStatement.setString(2, blankChildId);
+            ResultSet resultSet = preparedStatement.executeQuery();
+            boolean hasResults = resultSet.next();
+            List<String> rv = new ArrayList<>();
+            if (!hasResults) {
+                return rv;
+            }
+            do {
+                String key = resultSet.getString(1);
+                if (key != null) {
+                    rv.add(key);
+                }
+            } while (resultSet.next());
+            return rv;
+        } catch (SQLException e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+    @Override
+    public Map<String, String> getAllChildren(String path) {
+        try (Connection connection = getConnection()) {
+            PreparedStatement preparedStatement = connection.prepareStatement(getChildrenTemplate);
+            preparedStatement.setString(1, path);
+            preparedStatement.setString(2, blankChildId);
+            ResultSet resultSet = preparedStatement.executeQuery();
+            boolean hasResults = resultSet.next();
+            Map<String, String> rv = new HashMap<>();
+            if (!hasResults) {
+                return rv;
+            }
+            do {
+                String key = resultSet.getString(1);
+                String val = blobToValue(resultSet.getBlob(2));
+                if (val != null) {
+                    rv.put(key, val);
+                }
+            } while (resultSet.next());
+            return rv;
+        } catch (SQLException | LZFException e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+    @Override
+    public void close() {
+        cpds.close();
+    }
+}
---
+++ hydra-main/src/test/java/com/addthis/hydra/job/MysqlTest.java
@@ -0,0 +1,151 @@
+package com.addthis.hydra.job;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+import java.util.Properties;
+
+import com.addthis.hydra.job.store.MysqlDataStore;
+import com.addthis.hydra.job.store.MysqlInsertPickLastDataStore;
+import com.addthis.hydra.job.store.SpawnDataStore;
+import com.addthis.maljson.JSONObject;
+
+import com.google.common.collect.ImmutableList;
+import com.google.common.collect.ImmutableMap;
+import com.google.common.collect.ImmutableSet;
+
+import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertNull;
+
+public class MysqlTest {
+
+    private static final String user = "spawn";
+    private static final String password = "pw";
+
+
+    @Test
+    public void localTest() throws Exception {
+        Properties properties = new Properties();
+        properties.setProperty("user", user);
+        properties.setProperty("password", password);
+        String tableName = "newTable";
+        MysqlInsertPickLastDataStore ds = new MysqlInsertPickLastDataStore("jdbc:mysql:thin://localhost:3306/test", tableName, properties, false);
+        cleanupTestDataStore(ds);
+
+        perfTest(ds);
+        ds.close();
+    }
+
+    @Test
+    public void localTest2() throws Exception {
+        Properties properties = new Properties();
+        properties.setProperty("user", user);
+        properties.setProperty("password", password);
+        String tableName = "newTable";
+        MysqlInsertPickLastDataStore ds = new MysqlInsertPickLastDataStore("jdbc:mysql:thin://localhost:3306/test", tableName, properties, false);
+        for (int i=0; i<30; i++) {
+            ds.putAsChild("a", "b", "ver" + (30-i));
+            ds.putAsChild("a", "c", (30-i) + "ver");
+        }
+        ds.close();
+    }
+
+    private void cleanupTestDataStore(MysqlInsertPickLastDataStore ds) throws Exception {
+        int versions = 50;
+        for (int i=0; i<versions; i++) {
+            ds.put("key", "ver" + i);
+
+        }
+        System.out.println(ds.countRows() + " rows");
+        ds.cleanUp();
+        System.out.println(ds.countRows() + " rows");
+        assertEquals(ds.get("key"), "ver" + (versions - 1));
+    }
+
+    private static String bigJson = "";
+
+
+    @Test
+    public void remotePerfTest() throws Exception {
+        JSONObject obj = new JSONObject();
+        for (int i=0; i<1000; i++) {
+            obj.put("k" + i, "v" + i);
+        }
+        bigJson = obj.toString();
+        Properties properties = new Properties();
+        properties.setProperty("user", "spawn");
+        properties.setProperty("password", "rPmuq3sPfK9Ob0BY");
+        MysqlDataStore ds1 = new MysqlDataStore("ldm15f", 3306, "spawndatastore", "perftest2", properties);
+        MysqlInsertPickLastDataStore ds2 = new MysqlInsertPickLastDataStore("jdbc:mysql:thin://ldm15f:3306/spawndatastore", "test", properties, false);
+        for (int i=0; i<4; i++) {
+            perfTest(ds1);
+            perfTest(ds2);
+        }
+        ds1.close();
+        ds2.close();
+    }
+
+    private void perfTest(SpawnDataStore ds) throws Exception {
+        long start = System.currentTimeMillis();
+        ds.put("a", "old");
+        ds.put("a", "new");
+        ds.put("c", bigJson);
+        ds.putAsChild("p", "b", "old");
+        ds.putAsChild("p", "b", "new");
+        ds.putAsChild("p", "c", "new2");
+        System.out.println(ds.getDescription() + " write time: " + (System.currentTimeMillis() - start));
+        start = System.currentTimeMillis();
+        assertEquals("new", ds.get("a"));
+        assertNull(ds.get("b"));
+        assertEquals(ImmutableSet.of("b", "c"), ImmutableSet.copyOf(ds.getChildrenNames("p")));
+        assertEquals(ImmutableMap.of("b", "new", "c", "new2"), ds.getAllChildren("p"));
+        System.out.println(ds.getDescription() + " read time: " + (System.currentTimeMillis() - start));
+        start = System.currentTimeMillis();
+        ds.delete("a");
+        ds.delete("c");
+        ds.deleteChild("p", "b");
+        ds.deleteChild("p", "c");
+        System.out.println(ds.getDescription() + " delete time: " + (System.currentTimeMillis()-start));
+    }
+
+    private void correctnessTestDataStore(SpawnDataStore spawnDataStore) throws Exception {
+        String key1 = "key1";
+        String val1 = "value1";
+        String key2 = "key2";
+        String val2 = "value!!\"{ !!'' }[,'],;';;'\n_\\";
+
+        spawnDataStore.put(key1, "old1");
+        spawnDataStore.delete(key1);
+        spawnDataStore.put(key1, "old2");
+        spawnDataStore.put(key1, val1);
+        spawnDataStore.put(key2, val2);
+        assertNull("should get null for non-inserted key", spawnDataStore.get("key5"));
+        assertEquals("should get latest value", val1, spawnDataStore.get(key1));
+        assertEquals("should correctly fetch value with extra characters", val2, spawnDataStore.get(key2));
+        Map<String, String> expected = ImmutableMap.of(key1, val1, key2, val2);
+
+        String nullKey = "nullkey";
+        spawnDataStore.put(nullKey, "val");
+        spawnDataStore.put(nullKey, null);
+        assertNull("should get null for key inserted as null", spawnDataStore.get(nullKey));
+
+        assertEquals("should get expected map from multi-fetch call", expected, spawnDataStore.get(new String[]{key1, key2, "otherKey", "other'Key\nwithWeird;;';Characters"}));
+        spawnDataStore.putAsChild("parent", "child1", val1);
+        spawnDataStore.putAsChild("parent", "child2", "val2");
+        spawnDataStore.deleteChild("parent", "child2");
+        spawnDataStore.putAsChild("parent", "child3", val2);
+        assertEquals("should get expected child value", spawnDataStore.getChild("parent", "child1"), val1);
+        spawnDataStore.put("parent", "parentvalue");
+        List<String> expectedChildren = ImmutableList.of("child1", "child3");
+        assertEquals("should get expected children list", expectedChildren, spawnDataStore.getChildrenNames("parent"));
+        assertEquals("should get correct parent value", "parentvalue", spawnDataStore.get("parent"));
+        Map<String, String> expectedChildrenMap = ImmutableMap.of("child1", val1, "child3", val2);
+        assertEquals("should get expected children map", expectedChildrenMap, spawnDataStore.getAllChildren("parent"));
+        assertEquals("should get empty list for non-existent parent", new ArrayList<String>(), spawnDataStore.getChildrenNames("PARENT_NO_EXIST"));
+        assertEquals("should get empty map for non-existent parent", new HashMap<String, String>(), spawnDataStore.getAllChildren("PARENT_NO_EXIST"));
+    }
+
+}
