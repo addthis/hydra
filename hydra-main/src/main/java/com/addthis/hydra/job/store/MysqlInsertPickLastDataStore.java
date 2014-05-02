a1776b49fedf1d0b0457c1c72adfad4457f518f0
al <al@addthis.com>
2014-05-02 09:54:09 -0400

    Import timing/checking features from old mysql data store

--- hydra-main/src/main/java/com/addthis/hydra/job/store/MysqlInsertPickLastDataStore.java
+++ hydra-main/src/main/java/com/addthis/hydra/job/store/MysqlInsertPickLastDataStore.java
@@ -20,12 +20,17 @@ import com.mchange.v2.c3p0.ComboPooledDataSource;
 import com.ning.compress.lzf.LZFDecoder;
 import com.ning.compress.lzf.LZFEncoder;
 import com.ning.compress.lzf.LZFException;
+import com.yammer.metrics.Metrics;
+import com.yammer.metrics.core.Timer;
+import com.yammer.metrics.core.TimerContext;
 
 import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 public class MysqlInsertPickLastDataStore implements SpawnDataStore {
-    private static final Logger log = org.slf4j.LoggerFactory.getLogger(MysqlInsertPickLastDataStore.class);
+    private static final Logger log = LoggerFactory.getLogger(MysqlInsertPickLastDataStore.class);
 
+    private static final String driverClass = Parameter.value("sql.datastore.driverclass", "org.drizzle.jdbc.DrizzleDriver");
     private static final String description = "mysqlinsertpicklast";
     private final ComboPooledDataSource cpds;
     /* The maximum allowable length for 'path' and 'child' values. */
@@ -49,16 +54,22 @@ public class MysqlInsertPickLastDataStore implements SpawnDataStore {
     private final String getChildrenTemplate;
     private final String tableName;
 
+    /* Performance metrics */
+    private static final Timer queryTimer = Metrics.newTimer(MysqlInsertPickLastDataStore.class, "mysqlQueryTime");
+    private static final Timer insertTimer = Metrics.newTimer(MysqlInsertPickLastDataStore.class, "mysqlInsertTime");
+
     public MysqlInsertPickLastDataStore(String jdbcUrl, String tableName, Properties properties) throws Exception {
         cpds = new ComboPooledDataSource();
-        cpds.setDriverClass("org.drizzle.jdbc.DrizzleDriver");
+        cpds.setDriverClass(driverClass);
         cpds.setJdbcUrl(jdbcUrl);
         cpds.setInitialPoolSize(minPoolSize);
         cpds.setMinPoolSize(minPoolSize);
         cpds.setMaxPoolSize(maxPoolSize);
         cpds.setProperties(properties);
         this.tableName = tableName;
+        log.info("Connecting to mysql data table url={} table={} ", jdbcUrl, tableName);
         runStartupCommand();
+        /* Initialize templates. Done in constructor so they can be final */
         queryTemplate = String.format("SELECT %s FROM %s WHERE %s=? AND %s=? ORDER BY %s DESC LIMIT 1", valueKey, tableName, pathKey, childKey, idKey);
         insertTemplate = String.format("REPLACE INTO %s (%s,%s,%s) VALUES(?,?,?)", tableName, pathKey, valueKey, childKey);
         deleteTemplate = String.format("DELETE FROM %s WHERE %s=? AND %s=?", tableName, pathKey, childKey);
@@ -67,29 +78,44 @@ public class MysqlInsertPickLastDataStore implements SpawnDataStore {
                 childKey, valueKey, tableName, pathKey, childKey, idKey, idKey, tableName, childKey, childKey, childKey);
     }
 
-    public int countRows() throws SQLException {
-        try (Connection connection = getConnection()) {
-            ResultSet resultSet = connection.prepareStatement("SELECT COUNT(*) FROM " + tableName).executeQuery();
-            resultSet.next();
-            return resultSet.getInt(1);
+    private static void checkValidKeys(String key1, String key2) {
+        checkValidKey(key1);
+        checkValidKey(key2);
+    }
+
+    private static void checkValidKey(String key) {
+        if (key == null || blankChildId.equals(key) || key.length() > maxPathLength) {
+            throw new IllegalArgumentException("Invalid row key " + key + ": must be non-null and fewer than " + maxPathLength + " characters and not internal value " + blankChildId);
         }
     }
 
     private void runStartupCommand() throws SQLException {
-        try (Connection connection = getConnection()) {
-            connection.prepareStatement("CREATE TABLE IF NOT EXISTS " + tableName + "( "
-                                        + idKey + " MEDIUMINT NOT NULL AUTO_INCREMENT, "
-                                        + pathKey + " VARCHAR(" + maxPathLength + ") NOT NULL, "
-                                        + valueKey + " MEDIUMBLOB, "
-                                        + childKey + " VARCHAR(" + maxPathLength + "), "
-                                        + "PRIMARY KEY (" + idKey + "))"
-            ).execute();
+        try (Connection connection = cpds.getConnection()) {
+            String cmd = String.format("CREATE TABLE IF NOT EXISTS %s ( %s INT NOT NULL AUTO_INCREMENT, " +
+                          "%s VARCHAR(%d) NOT NULL, %s MEDIUMBLOB, %s VARCHAR(%d), PRIMARY KEY (%s))",
+                    tableName, idKey, pathKey, maxPathLength, valueKey, childKey, maxPathLength, idKey);
+
+            connection.prepareStatement(cmd).execute();
             connection.prepareStatement("ALTER TABLE " + tableName + " ADD UNIQUE(" + pathKey + "," + childKey + ")").execute();
         }
     }
 
-    protected Connection getConnection() throws SQLException {
-        return cpds.getConnection();
+    private static ResultSet executeAndTimeQuery(PreparedStatement preparedStatement) throws SQLException {
+        TimerContext timerContext = queryTimer.time();
+        try {
+            return preparedStatement.executeQuery();
+        } finally {
+            timerContext.stop();
+        }
+    }
+
+    private static boolean executeAndTimeInsert(PreparedStatement preparedStatement) throws SQLException {
+        TimerContext timerContext = insertTimer.time();
+        try {
+            return preparedStatement.execute();
+        } finally {
+            timerContext.stop();
+        }
     }
 
     @Override
@@ -99,89 +125,118 @@ public class MysqlInsertPickLastDataStore implements SpawnDataStore {
 
     @Override
     public String get(String path) {
+        checkValidKey(path);
         try {
-            return query(path, blankChildId);
-        } catch(NullPointerException npe ) {
+            return querySingleResult(path, blankChildId);
+        } catch(NullPointerException npe) {
+            /* Under some conditions a value set to null can cause an NPE in drizzle */
             return null;
         } catch (SQLException e) {
             throw new RuntimeException(e);
         }
     }
 
-    protected static Blob valueToBlob(String value) throws SQLException {
+    private static Blob valueToBlob(String value) throws SQLException {
         return value != null ? new SerialBlob(LZFEncoder.encode(value.getBytes())) : null;
     }
 
-    protected static String blobToValue(Blob blob) throws SQLException, LZFException {
+    private static String blobToValue(Blob blob) throws SQLException, LZFException {
         return blob != null ? new String(LZFDecoder.decode(blob.getBytes(1l, (int) blob.length()))) : null;
     }
 
     @Override
     public Map<String, String> get(String[] paths) {
-        // WHERE PATH=? OR PATH=? OR PATH=? IS FASTER
+        if (paths == null) {
+            return null;
+        }
+        // Query rows with blank childId. Use 'WHERE path IN (s1, s2, ...)' syntax, which performs better than a series of ORs
+        StringBuilder sb = new StringBuilder().append(String.format("SELECT %s,%s FROM %s WHERE %s=? AND %s IN (?", pathKey, valueKey, tableName, childKey, pathKey));
+        for (int i=0; i<paths.length-1; i++) {
+            sb.append(",?");
+        }
+        String command = sb.append(")").toString();
         Map<String, String> rv = new HashMap<>();
-        for (String path : paths) {
-            String val = get(path);
-            if (val != null) {
-                rv.put(path, val);
+        try (Connection conn = cpds.getConnection()) {
+            PreparedStatement preparedStatement = conn.prepareStatement(command);
+            preparedStatement.setString(1, blankChildId);
+            int j=2;
+            for (String path : paths) {
+                checkValidKey(path);
+                preparedStatement.setString(j++, path);
             }
+            ResultSet resultSet = executeAndTimeQuery(preparedStatement);
+            while (resultSet.next()) {
+                rv.put(resultSet.getString(pathKey), blobToValue(resultSet.getBlob(valueKey)));
+            }
+        } catch (SQLException | LZFException e) {
+            throw new RuntimeException(e);
         }
         return rv;
     }
 
     @Override
     public void put(String path, String value) throws Exception {
+        checkValidKey(path);
         insert(path, blankChildId, value);
     }
 
     private void insert(String path, String childId, String value)  throws SQLException {
-        try (Connection connection = getConnection()) {
+        try (Connection connection = cpds.getConnection()) {
             PreparedStatement preparedStatement = connection.prepareStatement(insertTemplate);
             preparedStatement.setString(1, path);
             preparedStatement.setBlob(2, valueToBlob(value));
             preparedStatement.setString(3, childId);
-            preparedStatement.execute();
+            executeAndTimeInsert(preparedStatement);
         }
     }
 
-    private String query(String path, String childId) throws SQLException {
-        try (Connection connection = getConnection()) {
+    private String querySingleResult(String path, String childId) throws SQLException {
+        try (Connection connection = cpds.getConnection()) {
             PreparedStatement preparedStatement = connection.prepareStatement(queryTemplate);
             preparedStatement.setString(1, path);
             preparedStatement.setString(2, childId);
-            ResultSet resultSet = preparedStatement.executeQuery();
+            ResultSet resultSet = executeAndTimeQuery(preparedStatement);
+            String firstValue;
             boolean hasData = resultSet.next();
             if (!hasData) {
                 return null;
             } else {
-                return blobToValue(resultSet.getBlob(1));
+                firstValue = blobToValue(resultSet.getBlob(1));
             }
+            boolean moreResults = resultSet.next();
+            if (moreResults) {
+                throw new RuntimeException("Found multiple results after expecting a unique result; bailing");
+            }
+            return firstValue;
         } catch (LZFException e) {
             throw new RuntimeException(e);
         }
     }
 
     private void delete(String path, String childId) throws SQLException {
-        try (Connection connection = getConnection()) {
+        try (Connection connection = cpds.getConnection()) {
             PreparedStatement preparedStatement = connection.prepareStatement(deleteTemplate);
             preparedStatement.setString(1, path);
             preparedStatement.setString(2, childId);
-            preparedStatement.execute();
+            executeAndTimeInsert(preparedStatement);
         }
     }
 
     @Override
     public void putAsChild(String parent, String childId, String value) throws Exception {
+        checkValidKeys(parent, childId);
         insert(parent, childId, value);
     }
 
     @Override
     public String getChild(String parent, String childId) throws Exception {
-        return query(parent, childId);
+        checkValidKeys(parent, childId);
+        return querySingleResult(parent, childId);
     }
 
     @Override
     public void deleteChild(String parent, String childId) {
+        checkValidKeys(parent, childId);
         try {
             delete(parent, childId);
         } catch (SQLException e) {
@@ -192,6 +247,7 @@ public class MysqlInsertPickLastDataStore implements SpawnDataStore {
     @Override
     public void delete(String path) {
         try {
+            checkValidKey(path);
             delete(path, blankChildId);
         } catch (SQLException e) {
             throw new RuntimeException(e);
@@ -200,11 +256,12 @@ public class MysqlInsertPickLastDataStore implements SpawnDataStore {
 
     @Override
     public List<String> getChildrenNames(String path) {
-        try (Connection connection = getConnection()) {
+        checkValidKey(path);
+        try (Connection connection = cpds.getConnection()) {
             PreparedStatement preparedStatement = connection.prepareStatement(getChildNamesTemplate);
             preparedStatement.setString(1, path);
             preparedStatement.setString(2, blankChildId);
-            ResultSet resultSet = preparedStatement.executeQuery();
+            ResultSet resultSet = executeAndTimeQuery(preparedStatement);
             boolean hasResults = resultSet.next();
             List<String> rv = new ArrayList<>();
             if (!hasResults) {
@@ -224,11 +281,12 @@ public class MysqlInsertPickLastDataStore implements SpawnDataStore {
 
     @Override
     public Map<String, String> getAllChildren(String path) {
-        try (Connection connection = getConnection()) {
+        checkValidKey(path);
+        try (Connection connection = cpds.getConnection()) {
             PreparedStatement preparedStatement = connection.prepareStatement(getChildrenTemplate);
             preparedStatement.setString(1, path);
             preparedStatement.setString(2, blankChildId);
-            ResultSet resultSet = preparedStatement.executeQuery();
+            ResultSet resultSet = executeAndTimeQuery(preparedStatement);
             boolean hasResults = resultSet.next();
             Map<String, String> rv = new HashMap<>();
             if (!hasResults) {
