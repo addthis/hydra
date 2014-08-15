7ce88fc7cddf226349a661b5cd4429abf238c1e4
ian <ian@addthis.com>
2014-08-15 18:05:23 -0400

    fix annoying stack trace from bad tests

--- hydra-main/src/main/java/com/addthis/hydra/query/MeshQueryMaster.java
+++ hydra-main/src/main/java/com/addthis/hydra/query/MeshQueryMaster.java
@@ -144,7 +144,9 @@ public class MeshQueryMaster extends ChannelOutboundHandlerAdapter {
                 spawnDataStoreHandler.close();
             }
             meshy.close();
-            tracker.close();
+            if (tracker != null) {
+                tracker.close();
+            }
         } catch (Exception e) {
             log.error("arbitrary exception during mqmaster shutdown", e);
         }
