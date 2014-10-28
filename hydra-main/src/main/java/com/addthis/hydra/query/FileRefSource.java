574924c748d28d0888e7d69d00ec99a0203aeed1
ian <ian@addthis.com>
2014-10-28 18:11:29 -0400

    fixes and improvements for query file ref gathers
    FileRefSource incorrectly assumed that only one thread could
    (concurrently) call meshy's receiveReference method. If the
    tryLock failed, it was assumed that the lock owner was the
    consumer thread in the middle of short-circuiting and so file
    refs were dropped. However, it was more likely to have been
    another file ref gatherer.
    
    Using a blocking lock would resolve, but it is not desirable to
    block netty event threads even for small amounts of time. Therefore,
    the class has been restructured with the most high-performance,
    albeit confusing, concurrency support I could think of. No mutexes,
    consumer thread does concurrent work, netty event threads do less
    work, no sleeping, no naive spin-locking, etc. It is probably
    faster than even the previous, naive, broken version.
    
    Additionally, there is some extra logging, and in particular, it
    will show responders who missed the short circuit time out. (on
    a per request basis).

--- hydra-main/src/main/java/com/addthis/hydra/query/FileRefSource.java
+++ hydra-main/src/main/java/com/addthis/hydra/query/FileRefSource.java
@@ -15,7 +15,8 @@ package com.addthis.hydra.query;
 
 import javax.annotation.concurrent.GuardedBy;
 
-import java.util.concurrent.CountDownLatch;
+import java.util.Queue;
+import java.util.concurrent.BlockingQueue;
 import java.util.concurrent.TimeUnit;
 import java.util.concurrent.locks.StampedLock;
 
@@ -25,59 +26,96 @@ import com.addthis.meshy.ChannelMaster;
 import com.addthis.meshy.service.file.FileReference;
 import com.addthis.meshy.service.file.FileSource;
 
+import com.google.common.base.Joiner;
+import com.google.common.collect.ConcurrentHashMultiset;
 import com.google.common.collect.HashMultimap;
 import com.google.common.collect.Multimap;
 
+import io.netty.util.internal.PlatformDependent;
+
+/**
+ * Supports multi-producer (server-to-server) file ref collection in a lock-free manner. This class is only
+ * thread-safe for the meshy-facing components though, and should not be concurrently accessed from user code.
+ *
+ * It uses a lock-free multi-producer/ single-consumer queue implementation from netty with some extra logic to
+ * support blocking/timed polls (the netty version only implements {@link Queue} and not {@link BlockingQueue}).
+ * The implementation of the blocking strives to avoid random sleeping or spin-blocking by using a {@link StampedLock}
+ * as a sort of binary semaphore. This gets us the park/unpark semantics we want without having to either directly
+ * implement that logic or throw away the spin-then-park loop optimizations that doug lee et al already benchmarked.
+ *
+ * There may be a "better" way to accomplish this than to use a {@link StampedLock}, but its code seems to suggest
+ * that it does a pretty efficient job at handling the methods we expect to call at their respective frequencies.
+ */
 class FileRefSource extends FileSource {
-    static final long maxGetFileReferencesTime = Parameter.intValue("qmaster.maxListFilesTime", 60);
+    private static final long MAX_WAIT_SECONDS = Parameter.intValue("qmaster.maxListFilesTime", 60);
+    private static final long MAX_WAIT_NANOS = TimeUnit.SECONDS.toNanos(MAX_WAIT_SECONDS);
+    private static final FileReference END_SIGNAL = new FileReference("END_SIGNAL", 0, 0);
 
-    @GuardedBy("lock")
-    private final Multimap<Integer, FileReference> fileRefMap;
-    private final CountDownLatch gate;
-    private final StampedLock lock;
+    /** Safe for multiple producers, but only a single consumer (lock-free). Borrowed from netty. */
+    private final Queue<FileReference> references;
+    /** Only used to park the consumer thread while waiting for more references (lock-free). */
+    private final StampedLock semaphore;
 
-    @GuardedBy("lock")
-    private boolean shortCircuited;
+    private volatile boolean shortCircuited;
+    // uses shortCircuited to ensure visibility after lazy instantiation, but otherwise is thread-safe on its own
+    @GuardedBy("shortCircuited") private ConcurrentHashMultiset<String> lateFileRefFinds;
 
     FileRefSource(ChannelMaster master) {
         super(master);
-        this.lock = new StampedLock();
+        this.references = PlatformDependent.newMpscQueue();
+        this.semaphore = new StampedLock();
+        // throw away a writeLock so that we can immediately park the consumer
+        this.semaphore.writeLock();
         this.shortCircuited = false;
-        this.fileRefMap = HashMultimap.create();
-        this.gate = new CountDownLatch(1);
     }
 
+    /** Called for every received file reference. Note! This method may be called concurrently by many threads. */
     @Override public void receiveReference(FileReference ref) {
-        String[] tokens = ref.name.split("/");
-        Integer id = Integer.parseInt(tokens[3]);
-        long stamp = lock.tryWriteLock();
-        if (stamp == 0) {
-            // assume shortCircuited would be set after waiting anyway
-            return;
-        }
-        try {
-            if (!shortCircuited) {
-                fileRefMap.put(id, ref);
-            }
-        } finally {
-            lock.unlockWrite(stamp);
+        if (!shortCircuited) {
+            references.add(ref);
+            semaphore.tryUnlockWrite();
+        } else {
+            lateFileRefFinds.add(ref.getHostUUID());
+            log.debug("throwing away ref due to short circuit {}", ref);
         }
     }
 
-    @Override public void receiveComplete() throws Exception {
-        gate.countDown();
+    /** Called once after every call to receiveReference has finished. This method is called by one of many threads. */
+    @Override public void receiveComplete() {
+        references.add(END_SIGNAL);
+        semaphore.tryUnlockWrite();
+        log.debug("query file ref source - receive complete for {}", this);
+        if (shortCircuited) {
+            log.warn("Late File Finds for {}:\n{}", this, Joiner.on('\n').join(lateFileRefFinds.entrySet()));
+        }
     }
 
     public Multimap<Integer, FileReference> getWithShortCircuit() throws InterruptedException {
-        if (!gate.await(maxGetFileReferencesTime, TimeUnit.SECONDS)) {
-            log.warn("Timed out waiting for mesh file list");
+        Multimap<Integer, FileReference> fileRefMap = HashMultimap.create();
+        long end = System.nanoTime() + MAX_WAIT_NANOS;
+        long remaining = MAX_WAIT_NANOS;
+        while (true) {
+            if (semaphore.tryWriteLock(remaining, TimeUnit.NANOSECONDS) == 0) {
+                lateFileRefFinds = ConcurrentHashMultiset.create();
+                shortCircuited = true;
+                log.warn("Timed out waiting for mesh file list: {}. After waiting {}", this, MAX_WAIT_NANOS);
+                return fileRefMap;
+            }
+            FileReference fileReference = references.poll();
+            while (fileReference != null) {
+                if (fileReference == END_SIGNAL) {
+                    log.debug("Finished collecting file refs for: {}", this);
+                    return fileRefMap;
+                }
+                fileRefMap.put(getTaskId(fileReference), fileReference);
+                fileReference = references.poll();
+            }
+            remaining = end - System.nanoTime();
         }
-        long stamp = lock.writeLock();
-        try {
-            shortCircuited = true;
-            return fileRefMap;
-        } finally {
-            lock.unlockWrite(stamp);
-        }
+    }
+
+    private static int getTaskId(FileReference fileReference) {
+        String[] tokens = fileReference.name.split("/");
+        return Integer.parseInt(tokens[3]);
     }
 }
