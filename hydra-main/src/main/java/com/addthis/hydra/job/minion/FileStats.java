5dc258e458b906d2f80d9b653900526e98fd5c01
ian <ian@addthis.com>
2014-11-19 20:30:37 -0500

    stream directories during file stats
    directories with enormous numbers of files are bad to enumerate
    as a giant array

--- hydra-main/src/main/java/com/addthis/hydra/job/minion/FileStats.java
+++ hydra-main/src/main/java/com/addthis/hydra/job/minion/FileStats.java
@@ -14,20 +14,41 @@
 package com.addthis.hydra.job.minion;
 
 import java.io.File;
+import java.io.IOException;
+
+import java.nio.file.DirectoryStream;
+import java.nio.file.Files;
+import java.nio.file.Path;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 public class FileStats {
+    private static final Logger log = LoggerFactory.getLogger(FileStats.class);
 
     public long count;
     public long bytes;
 
     void update(File dir) {
+        try {
+            update(dir.toPath());
+        } catch (IOException e) {
+            log.warn("Exception while scanning task files; treating directory as empty", e);
+            count = 0;
+            bytes = 0;
+        }
+    }
+
+    void update(Path dir) throws IOException {
         if (dir != null) {
-            for (File file : dir.listFiles()) {
-                if (file.isDirectory()) {
-                    update(file);
-                } else if (file.isFile()) {
-                    count++;
-                    bytes += file.length();
+            try (DirectoryStream<Path> directoryStream = Files.newDirectoryStream(dir)) {
+                for (Path file : directoryStream) {
+                    if (Files.isDirectory(file)) {
+                        update(file);
+                    } else if (Files.isRegularFile(file)) {
+                        count++;
+                        bytes += Files.size(file);
+                    }
                 }
             }
         } else {
