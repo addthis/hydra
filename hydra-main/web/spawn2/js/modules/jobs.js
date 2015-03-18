8995a216c8452ff1588087239ef941b6cc7c2e54
Michael Spiegel <spiegel@addthis.com>
2015-03-18 13:40:07 -0400

    Bug fix. Show the count of the # of jobs to delete.

--- hydra-main/web/spawn2/js/modules/jobs.js
+++ hydra-main/web/spawn2/js/modules/jobs.js
@@ -626,7 +626,7 @@ function(
         deleteSelected:function(jobIds){
             var count = jobIds.length;
             var self=this;
-            Alertify.dialog.confirm("Are you sure you would like to DELETE " + (count > 1 ? " jobs" : " job") + "?", function (resp) {
+            Alertify.dialog.confirm("Are you sure you would like to DELETE " + count + " " + (count > 1 ? " jobs" : " job") + "?", function (resp) {
 
                 _.each(jobIds,function(jobId){
                     var job = self.get(jobId);
