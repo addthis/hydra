9f7c57c3e83bb21256ebb42dd578da90fa4b003b
Steve <donnelly@addthis.com>
2015-04-15 12:36:22 -0400

    removing old JSONBundleizer maljson implementation, replacing with codec

--- hydra-task/src/main/java/com/addthis/hydra/task/source/bundleizer/JSONBundleizer.java
+++ hydra-task/src/main/java/com/addthis/hydra/task/source/bundleizer/JSONBundleizer.java
@@ -13,16 +13,12 @@
  */
 package com.addthis.hydra.task.source.bundleizer;
 
+import java.io.IOException;
+
 import com.addthis.bundle.channel.DataChannelError;
 import com.addthis.bundle.core.Bundle;
-import com.addthis.bundle.core.BundleException;
 import com.addthis.bundle.core.BundleFormat;
-import com.addthis.bundle.value.ValueArray;
-import com.addthis.bundle.value.ValueFactory;
-import com.addthis.bundle.value.ValueObject;
-import com.addthis.maljson.JSONArray;
-import com.addthis.maljson.JSONException;
-import com.addthis.maljson.JSONObject;
+import com.addthis.bundle.core.Bundles;
 
 public class JSONBundleizer extends NewlineBundleizer {
 
@@ -30,59 +26,9 @@ public class JSONBundleizer extends NewlineBundleizer {
     public Bundle bundleize(Bundle next, String line) {
         BundleFormat format = next.getFormat();
         try {
-            JSONObject json = new JSONObject(line);
-            for (String key : json.keySet()) {
-                next.setValue(format.getField(key), getJSONValue(json, key));
-            }
-        } catch (JSONException | BundleException ex) {
+            return Bundles.decode(line);
+        } catch (IOException ex) {
             throw new DataChannelError(ex);
-        }
-        return next;
-    }
-
-    //copied with slight changes from DataSourceJSON
-
-    private ValueObject getJSONValue(JSONObject json, String key) throws BundleException {
-        try {
-            Object raw = json.opt(key);
-            if (raw == null) {
-                return null;
-            }
-            Class<?> clazz = raw.getClass();
-
-                /* unwrap JSONObject to ValueMap */
-            if (clazz == JSONObject.class) {
-                // TODO
-                throw new UnsupportedOperationException("TODO");
-            } else if (clazz == JSONArray.class) {
-                // TODO: still only one dimension supported
-                JSONArray jarr = (JSONArray) raw;
-                ValueArray arr = ValueFactory.createArray(jarr.length());
-                for (int i = 0; i < jarr.length(); i++) {
-                    arr.add(i, createPrimitiveBundle(jarr.opt(i).getClass(), jarr.opt(i)));
-                }
-                return arr;
-            } else {
-                return createPrimitiveBundle(clazz, raw);
-            }
-        } catch (Exception ex) {
-            throw new BundleException(ex);
-        }
-    }
-
-    private ValueObject createPrimitiveBundle(Class<?> clazz, Object raw) {
-        // it would be cool to not do this twice, but I gave up fighting with generics
-        //Class<?> clazz = raw.getClass();
-        if (clazz == Integer.class) {
-            return ValueFactory.create(((Integer) raw).longValue());
-        } else if (clazz == Long.class) {
-            return ValueFactory.create(((Long) raw).longValue());
-        } else if (clazz == Float.class) {
-            return ValueFactory.create(((Float) raw).doubleValue());
-        } else if (clazz == Double.class) {
-            return ValueFactory.create(((Double) raw).doubleValue());
-        } else {
-            return ValueFactory.create(raw.toString());
         }
     }
 }
