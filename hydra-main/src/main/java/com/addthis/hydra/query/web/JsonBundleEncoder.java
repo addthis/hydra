aca55fa69c914b377f6407a310a829bd241bc857
ian <ian@addthis.com>
2015-05-01 23:04:42 -0400

    better json format support for query output
    previously it would silently ignore many of the more complex value
    object types. This was unintuitive and needless. Future improvements
    should include better integration with the encoder to write directly to
    the buffer, but this isn't worse than the previous iteration's toString.

--- hydra-main/src/main/java/com/addthis/hydra/query/web/JsonBundleEncoder.java
+++ hydra-main/src/main/java/com/addthis/hydra/query/web/JsonBundleEncoder.java
@@ -14,17 +14,18 @@
 
 package com.addthis.hydra.query.web;
 
+import java.io.UncheckedIOException;
+
 import com.addthis.bundle.core.Bundle;
 import com.addthis.bundle.core.BundleField;
 import com.addthis.bundle.value.ValueObject;
+import com.addthis.codec.jackson.Jackson;
 
-import com.google.common.annotations.VisibleForTesting;
+import com.fasterxml.jackson.core.JsonProcessingException;
 
 import static com.addthis.hydra.query.web.HttpUtils.setContentTypeHeader;
 
 public class JsonBundleEncoder extends AbstractBufferingHttpBundleEncoder {
-
-    static final char[] hex = {'0', '1', '2', '3', '4', '5', '6', '7', '8', '9', 'a', 'b', 'c', 'd', 'e', 'f'};
 
     public JsonBundleEncoder() {
         super();
@@ -55,23 +56,15 @@ public class JsonBundleEncoder extends AbstractBufferingHttpBundleEncoder {
             if (o == null) {
                 continue;
             }
-            ValueObject.TYPE type = o.getObjectType();
-            if (type == ValueObject.TYPE.CUSTOM) {
+            if (o.getObjectType() == ValueObject.TYPE.CUSTOM) {
                 o = o.asCustom().asSimple();
-                type = o.getObjectType();
             }
-            switch (type) {
-                case INT:
-                case FLOAT:
-                    sendBuffer.append(o.toString());
-                    break;
-                case STRING:
-                    sendBuffer.append('"');
-                    sendBuffer.append(jsonEncode(o.toString()));
-                    sendBuffer.append('"');
-                    break;
-                default:
-                    break;
+            try {
+                // TODO: no worse than the preivous version, but should still be able to avoid the string alloc.
+                String encodedCopy = Jackson.defaultMapper().writeValueAsString(o.asNative());
+                sendBuffer.append(encodedCopy);
+            } catch (JsonProcessingException e) {
+                throw new UncheckedIOException(e);
             }
         }
         sendBuffer.append(']');
@@ -80,60 +73,5 @@ public class JsonBundleEncoder extends AbstractBufferingHttpBundleEncoder {
     @Override
     protected void appendResponseEndToString(StringBuilder sendBuffer) {
         sendBuffer.append("]");
-    }
-
-    /* convert string to json valid format */
-    @VisibleForTesting
-    static String jsonEncode(String s) {
-        char[] ca = s.toCharArray();
-        int alt = 0;
-        for (int i = 0; i < ca.length; i++) {
-            if (ca[i] < 48 || ca[i] > 90) {
-                alt++;
-            }
-        }
-        if (alt == 0) {
-            return s;
-        }
-        StringBuilder sb = new StringBuilder(ca.length + alt * 3);
-        for (int i = 0; i < ca.length; i++) {
-            char c = ca[i];
-            if (c > 47 && c < 91) {
-                sb.append(ca[i]);
-            } else {
-                switch (ca[i]) {
-                    case '"':
-                        sb.append("\\\"");
-                        break;
-                    case '\\':
-                        sb.append("\\\\");
-                        break;
-                    case '\t':
-                        sb.append("\\t");
-                        break;
-                    case '\n':
-                        sb.append("\\n");
-                        break;
-                    case '\r':
-                        sb.append("\\r");
-                        break;
-                    default:
-                        if (c < 32 || c > 255) {
-                            sb.append("\\u");
-                            long v = c;
-                            char[] cb = new char[4];
-                            for (int j = 0; j < 4; j++) {
-                                cb[3 - j] = hex[(int) (v & 0xf)];
-                                v >>= 4;
-                            }
-                            sb.append(cb);
-                        } else {
-                            sb.append(c);
-                        }
-                        break;
-                }
-            }
-        }
-        return sb.toString();
     }
 }
