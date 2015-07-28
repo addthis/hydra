ed027faa7259ddcd2a6481ddb5cacb010ce1b536
ian <ian@addthis.com>
2015-07-28 14:35:04 -0400

    strike one exiting executor service + bonus fixes
    most of the bonus "fixes" are just cleanup, but there was one or two
    real fixes -- the lock() method inside a try block comes to mind.

--- hydra-main/src/main/java/com/addthis/hydra/query/spawndatastore/AliasBiMap.java
+++ hydra-main/src/main/java/com/addthis/hydra/query/spawndatastore/AliasBiMap.java
@@ -13,6 +13,8 @@
  */
 package com.addthis.hydra.query.spawndatastore;
 
+import javax.annotation.Nonnull;
+import javax.annotation.Nullable;
 import javax.annotation.concurrent.ThreadSafe;
 
 import java.io.IOException;
@@ -23,6 +25,7 @@ import java.util.Collections;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
+import java.util.Objects;
 import java.util.concurrent.ExecutionException;
 import java.util.concurrent.locks.ReentrantLock;
 
@@ -33,7 +36,9 @@ import com.addthis.hydra.job.store.DataStoreUtil;
 import com.addthis.hydra.job.store.SpawnDataStore;
 
 import com.google.common.annotations.VisibleForTesting;
+import com.google.common.base.Throwables;
 
+import com.fasterxml.jackson.core.type.TypeReference;
 import com.fasterxml.jackson.databind.ObjectMapper;
 
 import org.slf4j.Logger;
@@ -47,20 +52,17 @@ import org.slf4j.LoggerFactory;
 public class AliasBiMap {
 
     private static final Logger log = LoggerFactory.getLogger(AliasBiMap.class);
+
     public static final String ALIAS_PATH = "/query/alias";
 
     /* This SpawnDataStore must be the same type (zookeeper/priam) between Spawn and Mqmaster. This should
      * be guaranteed by the implementation of DataStoreUtil. */
-    private SpawnDataStore spawnDataStore;
-
-    private HashMap<String, List<String>> alias2jobs;
-    private HashMap<String, String> job2alias;
-
-    private ObjectMapper mapper;
-
-    private final ReentrantLock mapLock = new ReentrantLock();
-
-    private AvailableCache<String> mapCache;
+    private final SpawnDataStore spawnDataStore;
+    private final HashMap<String, List<String>> alias2jobs;
+    private final HashMap<String, String> job2alias;
+    private final ObjectMapper mapper;
+    private final ReentrantLock mapLock;
+    private final AvailableCache<String> mapCache;
 
     public AliasBiMap() throws Exception {
         this(DataStoreUtil.makeCanonicalSpawnDataStore());
@@ -69,6 +71,7 @@ public class AliasBiMap {
 
     public AliasBiMap(SpawnDataStore spawnDataStore) {
         this.spawnDataStore = spawnDataStore;
+        mapLock = new ReentrantLock();
         mapper = new ObjectMapper();
         alias2jobs = new HashMap<>();
         job2alias = new HashMap<>();
@@ -79,23 +82,24 @@ public class AliasBiMap {
         /* The max size of the alias cache */
         int cacheSize = Parameter.intValue("alias.bimap.cache.size", 100);
         mapCache = new AvailableCache<String>(cacheRefresh, cacheExpire, cacheSize, 2) {
-            @Override
-            public String fetchValue(String id) {
-                return updateAlias(id, fetchAlias(id));
+            @Override public String fetchValue(String id) {
+                try {
+                    return updateAlias(id, AliasBiMap.this.spawnDataStore.getChild(ALIAS_PATH, id));
+                } catch (Exception e) {
+                    log.warn("Exception when fetching alias: {}", id, e);
+                    return updateAlias(id, null);
+                }
             }
         };
     }
 
     @VisibleForTesting
-    protected List<String> decodeAliases(Object data) {
-        if (data == null) {
-            return null;
-        }
+    protected List<String> decodeAliases(@Nonnull Object data) {
         try {
-            return mapper.readValue(data.toString(), List.class);
+            return mapper.readValue(data.toString(), new TypeReference<List<String>>() {});
         } catch (IOException e) {
-            log.warn("Failed to decode data: " + e);
-            return new ArrayList<>();
+            log.warn("Failed to decode data", e);
+            return new ArrayList<>(0);
         }
     }
 
@@ -108,7 +112,7 @@ public class AliasBiMap {
             alias2jobs.clear();
             job2alias.clear();
             Map<String, String> aliases = spawnDataStore.getAllChildren(ALIAS_PATH);
-            if (aliases == null || aliases.isEmpty()) {
+            if ((aliases == null) || aliases.isEmpty()) {
                 log.warn("No aliases found, unless this is on first cluster startup something is probably wrong");
                 return;
             }
@@ -131,7 +135,7 @@ public class AliasBiMap {
         try {
             updateAlias(alias, mapCache.get(alias));
         } catch (ExecutionException e) {
-            log.warn("Failed to refresh alias " + alias + ": ", e);
+            log.warn("Failed to refresh alias: {}", alias, e);
         }
     }
 
@@ -141,17 +145,17 @@ public class AliasBiMap {
      * @param alias The alias key to check
      * @return String The data that was updated (so the cache can be updated)
      */
-    private String updateAlias(String alias, String data) {
+    @Nullable private String updateAlias(String alias, @Nullable String data) {
         if (alias == null) {
             return data;
         }
-        if (data == null || data.isEmpty()) {
+        if ((data == null) || data.isEmpty()) {
             deleteAlias(alias);
             return data;
         }
         List<String> jobs = decodeAliases(data);
-        if (jobs.size() == 0) {
-            log.warn("no jobs for alias " + alias + ", ignoring " + alias);
+        if (jobs.isEmpty()) {
+            log.warn("no jobs for alias {}, ignoring {}", alias, alias);
             return data;
         }
         mapLock.lock();
@@ -162,21 +166,6 @@ public class AliasBiMap {
             mapLock.unlock();
         }
         return data;
-    }
-
-    /**
-     * Fetch the data for an alias from the SpawnDataStore
-     *
-     * @param alias The alias to fetch
-     * @return A String representing the encoded job list, or null if no alias data exists
-     */
-    private String fetchAlias(String alias) {
-        try {
-            return spawnDataStore.getChild(ALIAS_PATH, alias);
-        } catch (Exception e) {
-            log.warn("Exception when fetching alias " + alias + ": ", e);
-            return null;
-        }
     }
 
     /**
@@ -194,36 +183,22 @@ public class AliasBiMap {
     }
 
     /**
-     * Get an unmodifiable view of the current Job map
-     *
-     * @return A map describing jobId => alias
-     */
-    public Map<String, String> viewJobMap() {
-        mapLock.lock();
-        try {
-            return Collections.unmodifiableMap(job2alias);
-        } finally {
-            mapLock.unlock();
-        }
-    }
-
-    /**
      * Update the SpawnDataStore with a new alias value
      *
      * @param alias The alias to add/change
      * @param jobs  The jobs to store under that alias
      */
     public void putAlias(String alias, List<String> jobs) {
+        mapLock.lock();
         try {
-            mapLock.lock();
             alias2jobs.put(alias, jobs);
             job2alias.put(jobs.get(0), alias);
             StringWriter sw = new StringWriter();
             mapper.writeValue(sw, jobs);
             spawnDataStore.putAsChild(ALIAS_PATH, alias, sw.toString());
         } catch (Exception e) {
-            log.warn("failed to put alias: " + alias + ": " + e);
-            throw new RuntimeException(e);
+            log.warn("failed to put alias: {}", alias, e);
+            throw Throwables.propagate(e);
         } finally {
             mapLock.unlock();
         }
@@ -239,10 +214,10 @@ public class AliasBiMap {
         try {
             List<String> jobs = alias2jobs.get(alias);
             alias2jobs.remove(alias);
-            if (jobs != null && !jobs.isEmpty()) {
+            if ((jobs != null) && !jobs.isEmpty()) {
                 for (String job : jobs) {
                     String aliasVal = job2alias.get(job);
-                    if (aliasVal != null && aliasVal.equals(alias)) {
+                    if (Objects.equals(aliasVal, alias)) {
                         job2alias.remove(job);
                     }
                 }
