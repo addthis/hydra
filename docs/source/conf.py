79158c4c2b620f8332a9b645460e73af39aa1c70
Irene Cho <irene.cho@addthis.com>
2016-09-20 11:29:36 -0400

    Changed only "copyright (Copyright) something AddThis" to "... Oracle" with this year 2016 Didn't change other '... AddThis...' without copyright/Copyright in a sentence ex) Before: copyright = AddThis Copyright 20XX AddThis
    After:
    copyright = Oracle
    Copyright 2016 Oracle
    
    Ref T65625

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -55,7 +55,7 @@ master_doc = 'index'
 
 # General information about the project.
 project = u'Hydra'
-copyright = u'2014, AddThis'
+copyright = u'2016, Oracle'
 
 # The version info for the project you're documenting, acts as replacement for
 # |version| and |release|, also used in various other places throughout the
