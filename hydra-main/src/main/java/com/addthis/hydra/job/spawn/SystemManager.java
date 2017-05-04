7a4cebaadef39618b34b3d921620d7306c5ef36e
Ted Pearson <ted.pearson@oracle.com>
2017-05-04 17:50:45 -0400

    fix licenses

--- hydra-main/src/main/java/com/addthis/hydra/job/spawn/SystemManager.java
+++ hydra-main/src/main/java/com/addthis/hydra/job/spawn/SystemManager.java
@@ -1,13 +1,15 @@
 /*
- * The contents of this file are subject to the terms
- * of the Common Development and Distribution License
- * (the "License").  You may not use this file except
- * in compliance with the License.
- * 
- * You can obtain a copy of the license at
- * http://www.opensource.org/licenses/cddl1.php
- * See the License for the specific language governing
- * permissions and limitations under the License.
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
  */
 package com.addthis.hydra.job.spawn;
 
