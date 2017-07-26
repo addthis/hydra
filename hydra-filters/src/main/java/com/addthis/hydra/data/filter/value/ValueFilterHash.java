760fd0677e3bfac4ee114424596a99e895d8567f
Irene Cho <irene.cho@oracle.com>
2017-07-26 12:46:26 -0400

    add murmur hash to ValueFilterHash ref T69516

--- hydra-filters/src/main/java/com/addthis/hydra/data/filter/value/ValueFilterHash.java
+++ hydra-filters/src/main/java/com/addthis/hydra/data/filter/value/ValueFilterHash.java
@@ -24,6 +24,11 @@ import com.addthis.bundle.value.ValueObject;
 import com.addthis.codec.annotations.FieldConfig;
 import com.addthis.hydra.common.hash.PluggableHashFunction;
 
+import com.google.common.base.Charsets;
+import com.google.common.hash.HashCode;
+import com.google.common.hash.HashFunction;
+import com.google.common.hash.Hashing;
+
 import org.apache.commons.codec.binary.Hex;
 
 /**
@@ -68,6 +73,7 @@ public class ValueFilterHash extends AbstractValueFilter {
         }
         long hash = 0;
         String sv = ValueUtil.asNativeString(value);
+
         switch (type) {
             case 0:
                 hash = sv.hashCode();
@@ -99,6 +105,11 @@ public class ValueFilterHash extends AbstractValueFilter {
                 } catch (NoSuchAlgorithmException e) {
                     // ignore
                 }
+            case 5:
+                HashFunction hashAlgorithm = Hashing.murmur3_128();
+                HashCode hashCode = hashAlgorithm.newHasher().putString(sv, Charsets.UTF_8).hash();
+                hash = hashCode.asLong();
+                break;
             default:
                 throw new RuntimeException("Unknown hash type: " + type);
         }
