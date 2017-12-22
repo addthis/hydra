3691ee01fb380664f79ac287f36a70c68501eb9d
Ted Pearson <ted.pearson@oracle.com>
2017-12-22 15:55:36 -0600

    add HostLocation.getPriorityLevel

--- hydra-main/src/main/java/com/addthis/hydra/minion/HostLocation.java
+++ hydra-main/src/main/java/com/addthis/hydra/minion/HostLocation.java
@@ -1,5 +1,7 @@
 package com.addthis.hydra.minion;
 
+import com.addthis.hydra.job.spawn.HostLocationSummary;
+
 import com.fasterxml.jackson.annotation.JsonCreator;
 import com.fasterxml.jackson.annotation.JsonProperty;
 
@@ -36,6 +38,17 @@ public class HostLocation {
 
     public String getPhysicalHost() {
         return physicalHost;
+    }
+
+    public String getPriorityAd(HostLocationSummary.HostLocationPriorityLevel hlpl) {
+        if (hlpl == HostLocationSummary.HostLocationPriorityLevel.DATACENTER) {
+            return this.getDataCenter();
+        } else if (hlpl == HostLocationSummary.HostLocationPriorityLevel.RACK) {
+            return this.getRack();
+        } else {
+            // HOST or NONE or anything else
+            return this.getPhysicalHost();
+        }
     }
 
     @Override
